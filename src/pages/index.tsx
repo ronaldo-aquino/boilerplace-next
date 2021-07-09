7a64f09b44a3ec34aedaa6057740961ec33d0815
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-07-09 17:34:26 -0300

    Adicionado husy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-const bla = 'var'
-
 export default function Home({ title = 'React Avançado' }) {
   return (
     <div className={styles.container}>
