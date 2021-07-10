74306dc7c86a088bd969e849717ee8697d039d25
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-07-10 00:03:12 -0300

    Boiletplace completo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  webpack5: false,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
